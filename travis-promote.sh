c55357aac8e8b6995f5d2b67ee67d5450d226efa
Stéphane Bégaudeau <stephane.begaudeau@gmail.com>
2014-04-25 11:23:53 +0200

    Fix the build 8.
    Change-Id: I003a679afa916b2ea60eaed71750f3eddf66464f

--- travis-promote.sh
+++ travis-promote.sh
@@ -1,20 +1,23 @@
-#!/bin/bash
-echo "$TRAVIS_BUILD_DIR"
 echo "Build $TRAVIS_JOB_NUMBER"
-echo "Commit $TRAVIS_COMMIT [$TRAVIS_BRANCH]"
+echo "$TRAVIS_COMMIT-MESSAGE"
+echo "Git: $TRAVIS_COMMIT [$TRAVIS_BRANCH]"
 echo "Java version: $TRAVIS_JDK_VERSION"
 CURRENT_TAG=$(git name-rev --name-only --tags HEAD)
 if [ $CURRENT_TAG == 'undefined' ]
 then
     echo "Promoting an untagged build on dartdesigner.github.io/p2/nightly"
-	cd $TRAVIS_BUILD_DIR/Dart-Designer/repositories/org.obeonetwork.dsl.dart.repository/target
+	cd repositories/org.obeonetwork.dsl.dart.repository/target
 	git clone https://$GITHUB_TOKEN@github.com/dartdesigner/p2.git -b gh-pages
 	if [ -d p2/nightly ]
 	then
 	    rm -r p2/nightly
+	    echo "An old version of the nightly repository has been found and removed"
 	fi
+	echo "Creating the nighly repository"
 	cp -r repository p2/
 	mv p2/repository p2/nightly
+	echo "Nightly repository created"
+	ls p2/nightly
 	cd p2
 	git add .
 	git commit -m "$TRAVIS_COMMIT-MESSAGE"
