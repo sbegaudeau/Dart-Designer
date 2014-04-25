3b5179395aac785ee3d2a258b929904a5060bb26
Stéphane Bégaudeau <stephane.begaudeau@gmail.com>
2014-04-25 11:10:40 +0200

    Fix the build 7.
    Change-Id: I262220572f3fa0291ab4641cec5dadb91a24dc0d

--- travis-promote.sh
+++ travis-promote.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+echo "$TRAVIS_BUILD_DIR"
 echo "Build $TRAVIS_JOB_NUMBER"
 echo "Commit $TRAVIS_COMMIT [$TRAVIS_BRANCH]"
 echo "Java version: $TRAVIS_JDK_VERSION"
@@ -7,7 +9,7 @@ then
     echo "Promoting an untagged build on dartdesigner.github.io/p2/nightly"
 	cd $TRAVIS_BUILD_DIR/Dart-Designer/repositories/org.obeonetwork.dsl.dart.repository/target
 	git clone https://$GITHUB_TOKEN@github.com/dartdesigner/p2.git -b gh-pages
-	if [-d p2/nightly]
+	if [ -d p2/nightly ]
 	then
 	    rm -r p2/nightly
 	fi
